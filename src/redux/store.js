598e9e964442a0ef198e7cb43c180985afdc6019
JuliusB-ui <141216908+JuliusB-ui@users.noreply.github.com>
2024-07-23 14:33:00 +0800

    complete

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import {contactsReducer} from "./contactSlice";
 import {filterReducer} from "./filterSlice";
-// import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, persistStore } from "redux-persist";
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const persistedReducer=persistReducer(persistConfig, combineReducers({contacts: contactsReducer, filter: filterReducer}))
 
 export const store=configureStore({
-    reducer: {
-        contacts: contactsReducer,
-        filter: filterReducer,
-    },
+    reducer: persistedReducer,
 
-    // middleware: getDefaultMiddleware => getDefaultMiddleware({
-    //     serializableCheck: {
-    //         ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    //     }
-    // })
+    middleware: getDefaultMiddleware => getDefaultMiddleware({
+        serializableCheck: {
+            ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        }
+    })
 });
-// export const persistor=persistStore(store);
+export const persistor=persistStore(store);
